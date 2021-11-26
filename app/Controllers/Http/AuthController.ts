b902aa53121125b6ccbf09cf87820fa5c0f2ce1a
joze08 <riosjunior270@gmail.com>
2021-11-26 14:34:26 -0300

    Start

---
+++ app/Controllers/Http/AuthController.ts
@@ -0,0 +1,26 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/user';
+
+export default class AuthController {
+  public async register({ request, response }: HttpContextContract) {
+    //try {
+    const data = request.only(["email", "password"]);
+    const user = await User.create(data);
+    return user;
+    /*     } catch (error) {
+          response.status(500).send("Erro ao cadastrar o usuário");
+        } */
+  }
+
+  public async authenticate({ response, request, auth }: HttpContextContract) {
+    try {
+      const { email, password } = request.all();
+      const { token } = await auth.use('api').attempt(email, password);
+      const user = await User.findByOrFail("email", email);
+      return { token, user };
+
+    } catch (error) {
+      response.status(500).send("Erro ao logar usuário");
+    }
+  }
+}
---
+++ app/Controllers/Http/MessagesController.ts
@@ -0,0 +1,34 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Message from 'App/Models/Message'
+
+export default class MessagesController {
+  public async index({ }: HttpContextContract) {
+    const message = await Message.query().preload('user');
+    return message;
+  }
+
+  public async store({ request, auth }: HttpContextContract) {
+    const data = request.only(["texto"]);
+    const message = await Message.create({ ...data, userId: auth.user?.id });
+    return message;
+  }
+
+  public async show({ params }: HttpContextContract) {
+    const message = await Message.findOrFail(params.id);
+    return message;
+  }
+
+  public async update({ params, request }: HttpContextContract) {
+    const message = await Message.findOrFail(params.id);
+    const { texto } = request.only(["texto"]);
+    message.texto = texto;
+    await message.save();
+    return message;
+  }
+
+  public async destroy({ params }: HttpContextContract) {
+    const message = await Message.findOrFail(params.id);
+    await message.delete()
+    return message;
+  }
+}
